cad817812a88209cc97fd7d8b8f097f8a7f1f1b2
RoboMWM <robomwm@users.noreply.github.com>
2015-12-05 14:27:52 -0800

    Remove timestamp (GP timestamps for us already)

--- src/main/java/me/robomwm/gpcustomlogger/CommandLogger.java
+++ src/main/java/me/robomwm/gpcustomlogger/CommandLogger.java
@@ -8,15 +8,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import static org.bukkit.Bukkit.getServer;
 
 public class CommandLogger implements Listener
 {
     GriefPrevention gp = (GriefPrevention)getServer().getPluginManager().getPlugin("GriefPrevention");
-    private final SimpleDateFormat timestampFormat = new SimpleDateFormat("HH:mm");
 
     //Feature: log all slash commands GP doesn't log
     @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
@@ -25,16 +21,14 @@ public class CommandLogger implements Listener
 
         if (event.isCancelled())
         {
-            String timestamp = this.timestampFormat.format(new Date());
-            gp.AddLogEntry(timestamp + " (Cancelled) " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
+            gp.AddLogEntry("(Cancelled) " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
         }
         else
         {
             int cmdIndex = event.getMessage().indexOf(' ');
             if (cmdIndex < 0) //if command has no arguments, log
             {
-                String timestamp = this.timestampFormat.format(new Date());
-                gp.AddLogEntry(timestamp + " " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
+                gp.AddLogEntry(event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
                 return;
             }
             //Don't log whispers (already logged by GP)
@@ -45,8 +39,7 @@ public class CommandLogger implements Listener
                 return;
             else
             {
-                String timestamp = this.timestampFormat.format(new Date());
-                gp.AddLogEntry(timestamp + " " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
+                gp.AddLogEntry(event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
             }
         }
     }
@@ -57,8 +50,7 @@ public class CommandLogger implements Listener
     {
         if (event.isCancelled())
         {
-            String timestamp = this.timestampFormat.format(new Date());
-            gp.AddLogEntry(timestamp + " (Cancelled) " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
+            gp.AddLogEntry("(Cancelled) " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
         }
     }
 }
