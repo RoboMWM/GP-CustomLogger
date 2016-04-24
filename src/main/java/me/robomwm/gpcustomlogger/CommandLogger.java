23b14f87db8ed44ce2beb06c22d2e03c0ae52b69
RoboMWM <robomwm@users.noreply.github.com>
2016-04-23 19:24:10 -0700

    Account for more than 1 softmuted player

--- src/main/java/me/robomwm/gpcustomlogger/CommandLogger.java
+++ src/main/java/me/robomwm/gpcustomlogger/CommandLogger.java
@@ -17,7 +17,7 @@ public class CommandLogger implements Listener
     GriefPrevention gp = (GriefPrevention)getServer().getPluginManager().getPlugin("GriefPrevention");
 
     //Feature: log all slash commands GP doesn't log
-    @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event)
     {
 
@@ -47,7 +47,7 @@ public class CommandLogger implements Listener
     }
 
     //Feature: log all cancelled chat messages
-    @EventHandler(ignoreCancelled = false, priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR)
     void onPlayerChat(AsyncPlayerChatEvent event)
     {
         if (event.isCancelled())
@@ -55,22 +55,30 @@ public class CommandLogger implements Listener
             gp.AddLogEntry("(Cancelled) " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
             return;
         }
-        
+
         //Feature: log "spam-muted" chat messages
         //Don't waste time figuring out if a player was softmuted if they're the only one on the server
-        if (Bukkit.getOnlinePlayers().size() < 2)
+
+        int numberOfOnlinePlayers = Bukkit.getOnlinePlayers().size();
+
+        if (Bukkit.getOnlinePlayers().size() < numberOfOnlinePlayers)
             return;
 
-        //Is the player the only recipient?
+        //Is the player the only recipient? (Spam messages are only sent to recipient)
         if (event.getRecipients().size() == 1)
         {
-            //Is the player already softmuted? GP logs softmuted messages for us already
-            //However, we'll log softmuted messages anyways since GP doesn't log the initial message that auto-softmutes them
+            //Is the player already softmuted?
             if (gp.dataStore.isSoftMuted(event.getPlayer().getUniqueId()))
                 gp.AddLogEntry("(soft-muted) " + event.getPlayer().getName() + "> " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
             else
                 gp.AddLogEntry("(spam-muted) " + event.getPlayer().getName() + ": " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
-
+        }
+        //Otherwise is player softmuted?
+        //GP doesn't log the initial message that auto-softmutes players, so I decided to implement this feature myself.
+        else if (event.getRecipients().size() < numberOfOnlinePlayers)
+        {
+            if (gp.dataStore.isSoftMuted(event.getPlayer().getUniqueId())) //Make sure this guy is indeed softmuted (accounts for "group chat" plugins)
+                gp.AddLogEntry(numberOfOnlinePlayers + " (soft-muted) " + event.getPlayer().getName() + "> " + event.getMessage(), CustomLogEntryTypes.AdminActivity, true);
         }
     }
 
